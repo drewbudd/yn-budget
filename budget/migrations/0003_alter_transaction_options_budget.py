380147c450c501e3a3b9889a7a50ffdeeae3e9dd
Andrew Almaguer <andrew.almaguer@outlook.com>
2026-06-04 21:10:06 +0200

    Fix migration order: move budget options and Budget table creation to migration 0003

--- budget/migrations/0003_alter_transaction_options_budget.py
+++ budget/migrations/0003_alter_transaction_options_budget.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('transactions', '0001_initial'),
+        ('budget', '0002_alter_transaction_to_include_choice'),
     ]
 
     operations = [
